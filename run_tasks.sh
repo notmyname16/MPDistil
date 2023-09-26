120e9b411aedac3b50616b974307e0c8d2b142e5
Shantanu Dixit <88495622+notmyname16@users.noreply.github.com>
2023-09-26 11:53:07 +0530

    Update run_tasks.sh

--- run_tasks.sh
+++ run_tasks.sh
@@ -1,4 +1,4 @@
-for task in 'CB' 'WSC' 'RTE'
+for task in 'CB'
 do
     TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=0 CUDA_VISIBLE_DEVICES=0 python main_superglue.py --task $task --data_dir ./superglue_data/ --do_lower_case --wandb_logging --reward_function 'binary'
     TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=0 CUDA_VISIBLE_DEVICES=0 python main_superglue.py --task $task --data_dir ./superglue_data/ --do_lower_case --wandb_logging --use_comp_loss --reward_function 'binary'
